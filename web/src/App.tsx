3513fed8b288c6c9dad4f9ea98ce392ce2594fa5
ARTHURPC03 <arthurpimetelcordeiro@gmail.com>
2020-08-23 20:50:32 -0300

    Navigation and global style

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-function App() {
-  return <h1>Hello World</h1>
+import Routes from './routes'
+import Header from './components/Header'
+
+import GlobalStyle from './styles/global'
+
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Header />
+        <Routes />
+      </Router>
+    </>
+  )
 }
 
 export default App
